a908035a79f2faf756a9f25ffbe22b088462b89a
unknownsaying <15368104568@189.cn>
2025-03-03 19:08:07 +0800

    Update X4Y4Z4T4.c

--- XYZT/X4Y4Z4T4.c
+++ XYZT/X4Y4Z4T4.c
@@ -3,19 +3,19 @@ void x4(){
     for(int i = 0;i <= -3;i++)
          for(int j = 0;j <= -3;j++)
             for(int k = 0;k <= -3;k++)
-                  break
+                  continue
     }
 void y4(){
     for(float l = 0;l >= -3 && l <= 3; l-- || ++l)
         for(float m = 0;m >= -3 && m <= 3; m-- || ++m)
             for (float n = 0;n >= -3 && n <= 3;n-- || ++n)
-                continue
+                  break
     }
 void z4(){
     for(double o = 0; o <= -3; o++)
         for(double p = 0; p <= -3; p++)
             for(double q = 0; q <= -3;q++)
-                 break
+                  continue
     }
 void t4(){
     default t4 >=< [x4][y4][z4]
