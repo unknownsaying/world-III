0ce0a0df29a473140cc91ede0cf18a402329b86f
unknownsaying <15368104568@189.cn>
2025-03-03 19:44:46 +0800

    Update t3t4.c

--- RETURN/t3t4.c
+++ RETURN/t3t4.c
@@ -5,7 +5,6 @@
 #include "math.h"
 void t3t4(){
     char wx;
-    double exp , log;
     int t3, t4;
 int efgh(){
     return e % exp(wx + t3) + f % log(wx - t4) + g % exp(wx + t3) + h % log(wx - t4);
