583da8643eb0e74aebdf59c5f40f9074d513d882
unknownsaying <15368104568@189.cn>
2025-03-03 19:26:25 +0800

    Update txyz.c

--- XYZT/txyz.c
+++ XYZT/txyz.c
@@ -5,5 +5,5 @@
 #include "../XYZT/t5x5y5z5.c"
 #include "../XYZT/t6x6y6z6.c"
 typedef struct {
-         x1y1z1t1<->x2y2z2t2<->x3y3z3t3<->x4y4z4t4<->x5y5z5t5<->x6y6z6t6
+        return t1x1y1z1<->t2x2y2z2<->t3x3y3z3<->t4x4y4z4<->t5x5y5z5<->t6x6y6z6<->t1x1y1z1
 }
