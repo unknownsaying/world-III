f15877efe3b7032d9b06aafafeb1ebaaa2f05362
unknownsaying <15368104568@189.cn>
2025-03-03 19:48:54 +0800

    Update t11t12.c

--- RETURN/t11t12.c
+++ RETURN/t11t12.c
@@ -1,7 +1,6 @@
 #include "../R/rst369.c"
 void t11t12(){
     char www;
-    double sin , cos;
     int t11, t12;
 int rst(){
     return r & sin(www + t11) + s & cos(www - t12) + t & sin(www + t11) + uv & cos(www - t12);
