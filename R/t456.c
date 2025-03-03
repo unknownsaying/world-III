0c85260b641942a06a951655d8097271d228d545
unknownsaying <15368104568@189.cn>
2025-03-03 18:54:36 +0800

    Update t456.c

--- R/t456.c
+++ R/t456.c
@@ -1,13 +1,13 @@
 #define kji [t4][k][j][i]
 #define nml [t5][n][m][l]
 #define qpo [t6][q][p][o]
-void t456();{
+void rst456(){
     int i,j,k;
     float l,m,n;
     double o,p,q;
+}
 typedef struct {
     extern k -> j -> i -> t4;
     extern q -> p -> o -> t5;
     extern n -> m -> l -> t6;
-}
 }
