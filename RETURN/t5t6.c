c6cf46aa8da07adfa4b79d823dc08031edf2166d
unknownsaying <15368104568@189.cn>
2025-03-03 19:38:23 +0800

    Update t5t6.c

--- RETURN/t5t6.c
+++ RETURN/t5t6.c
@@ -6,9 +6,7 @@ void t5t6(){
     double asin , acos;
     int t5, t6;
 int ijk(){
-    return i / asin(wx + t5) + j / acos(wx - t6);
-    return k / asin(wx + t5) + l / acos(wx - t6);
-    return i / asin(wx - t5) + j / acos(wx + t6);
-    return k / asin(wx - t5) + l / acos(wx + t6);
+    return i / asin(wx + t5) + j / acos(wx - t6) + k / asin(wx + t5) + l / acos(wx - t6);
+    return i / asin(wx - t5) + j / acos(wx + t6) + k / asin(wx - t5) + l / acos(wx + t6);
 }
 }
