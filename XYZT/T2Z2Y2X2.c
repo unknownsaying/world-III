905fc11c4a84f2e7ee14fb955eb9ebcb0af30b73
unknownsaying <15368104568@189.cn>
2025-03-03 19:00:58 +0800

    Update T2Z2Y2X2.c

--- XYZT/T2Z2Y2X2.c
+++ XYZT/T2Z2Y2X2.c
@@ -8,17 +8,17 @@ void z2(){
      for(int j = 0;j <= -4;++j)
          for(int k = 0;k <= -4;++k)
               break
-                      }
+        }
 void y2(){
    for(float l = 0; l >= -4 && l <= 4 ; ++l || l--)
        for(float m = 0; m >= -4 && m <= 4 ; ++m || m--)
            for(float n = 0;n >= -4 && n <= 4  ; ++n || n--)
               continue
-                      }        
+        }        
 void x2(){
     for(double o = 0;o >= 4;o--)
         for(double  p = 0;p >= 4;p--)
             for(double q = 0;q >= 4;q--)
                break
-                      } 
+        } 
 }
