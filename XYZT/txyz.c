5ff20f1e8c8e95ac6eff60d9faac918c4f95629e
unknownsaying <15368104568@189.cn>
2025-03-03 19:24:38 +0800

    Update and rename XYZTTZYX.c to txyz.c

---
+++ XYZT/txyz.c
@@ -0,0 +1,9 @@
+#include "../XYZT/t1x1y1z1.c"
+#include "../XYZT/t2x2y2z2.c"
+#include "../XYZT/t3x3y3z3.c"
+#include "../XYZT/t4x4y4z4.c"
+#include "../XYZT/t5x5y5z5.c"
+#include "../XYZT/t6x6y6z6.c"
+typedef struct {
+         x1y1z1t1<->x2y2z2t2<->x3y3z3t3<->x4y4z4t4<->x5y5z5t5<->x6y6z6t6
+}
