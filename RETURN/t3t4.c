7a276e897e777b3d36b6ad85d34b3ed9c1b381c8
unknownsaying <15368104568@189.cn>
2025-03-03 19:44:20 +0800

    Update t3t4.c

--- RETURN/t3t4.c
+++ RETURN/t3t4.c
@@ -7,8 +7,8 @@ void t3t4(){
     char wx;
     double exp , log;
     int t3, t4;
-int efgh(EFGH){
-    return E % exp(wx + t3) + F % log(wx - t4) + G % exp(wx + t3) + H % log(wx - t4);
-    return e % exp(wx - t3) + f % log(wx + t4) + g % exp(wx - t3) + h % log(wx + t4);
+int efgh(){
+    return e % exp(wx + t3) + f % log(wx - t4) + g % exp(wx + t3) + h % log(wx - t4);
+    return E % exp(wx - t3) + F % log(wx + t4) + G % exp(wx - t3) + H  % log(wx + t4);
 } 
 }
