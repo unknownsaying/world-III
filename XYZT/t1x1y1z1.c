dc5f044ec369f5ee922d2d41499e97aa81ed9518
unknownsaying <15368104568@189.cn>
2025-03-03 19:33:38 +0800

    Update t1x1y1z1.c

--- XYZT/t1x1y1z1.c
+++ XYZT/t1x1y1z1.c
@@ -3,7 +3,7 @@ void t1(){
 default [t1] >=< [z1][y1][x1]
         [t1 != t2]<->[t2][t3][t4]
          } 
-void z1(){
+void x1(){
   for(int i = 0;i <= -2;++i)
      for(int j = 0;j <= -2;++j)
          for(int k = 0;k <= -2;++k)
@@ -15,7 +15,7 @@ void y1(){
            for(float n = 0;n >= -2 && n <= 2  ; ++n || --n)
               continue
                       }        
-void x1(){
+void z1(){
     for(double o = 0;o >= 2;--o)
         for(double  p = 0;p >= 2;--p)
             for(double q = 0;q >= 2;--q)
