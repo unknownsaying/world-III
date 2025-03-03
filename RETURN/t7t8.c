7358e7ae08460611ca80f18fda605122e3adbe2f
unknownsaying <15368104568@189.cn>
2025-03-03 19:43:42 +0800

    Update t7t8.c

--- RETURN/t7t8.c
+++ RETURN/t7t8.c
@@ -4,7 +4,7 @@ void t7t8(){
     char www;
     double ceil , floor;
     int t7, t8;
-void lmn(l){
+void lmn(){
     return l ^ ceil(www + t7) + m ^ floor(www - t8) + n ^ ceil(www + t7) + o ^ floor(www - t8);
     return L ^ ceil(www - t7) + M ^ floor(www + t8) + N ^ ceil(www - t7) + O ^ floor(www + t8);
 } 
