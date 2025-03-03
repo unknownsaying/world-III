3aa49be5a0c01b265e6d28d84230e10e96c642c1
unknownsaying <15368104568@189.cn>
2025-03-03 19:40:42 +0800

    Update t1t2.c

--- RETURN/t1t2.c
+++ RETURN/t1t2.c
@@ -7,8 +7,8 @@ void t1t2(){
     char wx;
     double sin,cos;
     int t1 , t2;
-int A B C D(){
+int ABCD(abcd){
     return A * sin(wx + t1) + B * cos(wx - t2) + C * sin(wx + t1) + D * cos(wx - t2);
-    return A * sin(wx - t1) + B * cos(wx + t2) + C * sin(wx - t1) + D * cos(wx + t2);
+    return a * sin(wx - t1) + b * cos(wx + t2) + c * sin(wx - t1) + d * cos(wx + t2);
 } 
 }
