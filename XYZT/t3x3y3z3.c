697d1e68ebb02ee5ed8dd3b9568013bd821f6bc1
unknownsaying <15368104568@189.cn>
2025-03-03 19:21:56 +0800

    Update and rename T3Z3Y3X3.c to t3x3y3z3.c

--- XYZT/t3x3y3z3.c
+++ XYZT/t3x3y3z3.c
@@ -1,12 +1,12 @@
-typedef struct t3z3y3x3 {
+typedef struct t3x3y3z3 {
 void t3(){
     default [t3] >=< [z3][y3][x3]
             [t3 != t4] <-> [t1][t2][t4]
         }
-void z3(){
-    for (int i = 0; i <= -8; i++)
-        for (int j = 0; j <= -8; j++)
-            for (int k = 0; k <= -8; k++)
+void x3(){
+    for (double o = 0; o <= 8; --o)
+        for (double p = 0; p <= 8; --p)
+           for (double q = 0; q <= 8; --q)
                 break
         }
 void y3(){
@@ -15,10 +15,10 @@ void y3(){
             for (float n = 0; n >= -8 && n <= 8; n-- || ++n)
                 continue
         }
-void x3(){
-    for (double o = 0; o <= 8; --o)
-        for (double p = 0; p <= 8; --p)
-           for (double q = 0; q <= 8; --q)
+void z3(){
+    for (int i = 0; i <= -8; i++)
+        for (int j = 0; j <= -8; j++)
+            for (int k = 0; k <= -8; k++)
                 break
         }
-    }
+}
