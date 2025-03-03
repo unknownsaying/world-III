9e8448563390fe34a6f78ea900bd152adb27c8cb
unknownsaying <15368104568@189.cn>
2025-03-03 19:39:40 +0800

    Update t9t10.c

--- RETURN/t9t10.c
+++ RETURN/t9t10.c
@@ -5,9 +5,7 @@ void t9t10(){
     double asin , acos;
     int t9, t10;
 void opq(){
-    return o :: sh(www + t9) + p :: ch(www - t10);
-    return q :: sh(www + t9) + o :: ch(www - t10);
-    return o :: sh(www - t9) + p :: ch(www + t10);
-    return q :: sh(www - t9) + k :: ch(www + t10);
+    return o :: sh(www + t9) + p :: ch(www - t10) + q :: sh(www + t9) + o :: ch(www - t10);
+    return o :: sh(www - t9) + p :: ch(www + t10) + q :: sh(www - t9) + o :: ch(www + t10);
 } 
 }
