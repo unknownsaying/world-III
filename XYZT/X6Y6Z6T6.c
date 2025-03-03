f55acfb23020802136cd3c870050618df4e9ca3f
unknownsaying <15368104568@189.cn>
2025-03-03 19:09:03 +0800

    Update X6Y6Z6T6.c

--- XYZT/X6Y6Z6T6.c
+++ XYZT/X6Y6Z6T6.c
@@ -3,19 +3,19 @@ void x6(){
     for(int i = 0;i <= -9 ;i++)
        for(int j = 0;j <= -9 ;j++)
            for(int k = 0;k <= -9;k++)
-              break
+                 continue
             }
 void y6(){
      for (float l = 0;l >= -9 && l <= 9; l++ || l--)
          for(float m = 0;m >= -9 && m <= 9; m++ || m--)
              for (float n = 0;n >= -9 && n <= 9; n++ || n--)
-                 continue
+                  break
                 }
 void z6(){
     for (double o = 0; o >= 9 ; o--)
         for(double p = 0; p >= 9 ; p--)
             for (double q = 0; q >= 9; q--)
-                break
+                 continue
                 }
 void t6(){
      default t6 >=< [x6][y6][z6]
