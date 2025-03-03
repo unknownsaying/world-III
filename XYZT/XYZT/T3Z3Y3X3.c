d19228d2e5da81713ff77638da30786805989ea5
unknownsaying <15368104568@189.cn>
2025-03-03 18:58:40 +0800

    Add files via upload

---
+++ XYZT/XYZT/T3Z3Y3X3.c
@@ -0,0 +1,24 @@
+typedef struct t3z3y3x3 {
+void t3(){
+    default [t3] >=< [z3][y3][x3]
+            [t3 != t4] <-> [t1][t2][t3]
+        }
+void z3(){
+    for (int i = 0; i <= -8; ++i)
+        for (int j = 0; j <= -8; ++j)
+            for (int k = 0; k <= -8; ++k)
+                break
+        }
+void y3(){
+    for (float l = 0; l >= -8 && l <= 8; ++l || l--)
+        for (float m = 0; m >= -8 && m <= 8; ++m || m--)
+            for (float n = 0; n >= -8 && n <= 8; ++n || n--)
+                continue
+        }
+void x3(){
+    for (double o = 0; o <= 8; o--)
+         for (double p = 0; p <= 8; p--)
+            for (double q = 0; q <= 8; q--)
+                break
+        }
+    }
---
+++ XYZT/XYZT/XYZTTZYX.c
@@ -0,0 +1,10 @@
+typedef struct {
+    void X Y Z T(){
+         x1y1z1t1<->x2y2z2t2<->x3y3z3t3
+         return t7
+    }
+    void T Z Y X(){
+        z4y4x4t4<->z5y5x5t5<->z6y6x6t6t6
+        return t7
+    }
+}
