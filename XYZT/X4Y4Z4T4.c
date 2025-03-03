f138ead47d2c82d37dbcd01344d8290220ac03ac
unknownsaying <15368104568@189.cn>
2025-03-03 19:06:22 +0800

    Update X4Y4Z4T4.c

--- XYZT/X4Y4Z4T4.c
+++ XYZT/X4Y4Z4T4.c
@@ -1,14 +1,14 @@
 typedef struct x4y4z4t4 {
 void x4(){
-    for(int i = 0;i < -3;i++)
-         for(int j = 0;j < -3;j++)
-            for(int k = 0;k < -3;k++)
+    for(int i = 0;i <= -3;i++)
+         for(int j = 0;j <= -3;j++)
+            for(int k = 0;k <= -3;k++)
                   break
     }
 void y4(){
-    for(float l = 0;l >>= 0 && l <<= -3; ++l || l--)
-        for(float m = 0;m >>= 0 && m <<= -3; ++m || m--)
-            for (float n = 0;n >>= 0 && n <<= -3;++n || n--)
+    for(float l = 0;l >= -3 && l <= 3; l-- || ++l)
+        for(float m = 0;m >= -3 && m <= 3; m-- || ++m)
+            for (float n = 0;n >= -3 && n <= 3;n-- || ++n)
                 continue
     }
 void z4(){
