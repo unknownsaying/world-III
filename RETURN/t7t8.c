3bc899ad8f42596f00048de400b408ab3db17f44
unknownsaying <15368104568@189.cn>
2025-03-03 19:39:12 +0800

    Update t7t8.c

--- RETURN/t7t8.c
+++ RETURN/t7t8.c
@@ -5,9 +5,7 @@ void t7t8(){
     double ceil , floor;
     int t7, t8;
 void lmn(){
-    return l ^ ceil(www + t7) + m ^ floor(www - t8);
-    return n ^ ceil(www + t7) + n ^ floor(www - t8);
-    return l ^ ceil(www - t7) + m ^ floor(www + t8);
-    return n ^ ceil(www - t7) + n ^ floor(www + t8);
+    return l ^ ceil(www + t7) + m ^ floor(www - t8) + n ^ ceil(www + t7) + n ^ floor(www - t8);
+    return l ^ ceil(www - t7) + m ^ floor(www + t8) + n ^ ceil(www - t7) + n ^ floor(www + t8);
 } 
 }
