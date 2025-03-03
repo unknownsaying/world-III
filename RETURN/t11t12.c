d3c7e69a160011c232897dd212941760fa860b17
unknownsaying <15368104568@189.cn>
2025-03-03 19:39:54 +0800

    Update t11t12.c

--- RETURN/t11t12.c
+++ RETURN/t11t12.c
@@ -4,9 +4,7 @@ void t11t12(){
     double sin , cos;
     int t11, t12;
 int rst(){
-    return r & sin(www + t11) + s & cos(www - t12);
-    return t & sin(www + t11) + uv & cos(www - t12);
-    return r & sin(www - t11) + s & cos(www + t12);
-    return t & sin(www - t11) + uv & cos(www + t12);
+    return r & sin(www + t11) + s & cos(www - t12) + t & sin(www + t11) + uv & cos(www - t12);
+    return r & sin(www - t11) + s & cos(www + t12) + t & sin(www - t11) + uv & cos(www + t12);
 } 
 }
