62a8bc79711739e5b39c9802fa7495f3f2289d40
unknownsaying <15368104568@189.cn>
2025-10-13 21:04:53 +0800

    Update t5t6.c

--- RETURN/t5t6.c
+++ RETURN/t5t6.c
@@ -1,11 +1,9 @@
-#include "../R/rst123.c"
-#include "../crak/ijk.c"
 #include "math.h"
 void t5t6(){
-    char wx;
+    char xz;
     int t5, t6;
 int ijk(){
-    return I / asin(wx + t5) + J / acos(wx - t6) + K / asin(wx + t5) + L / acos(wx - t6);
-    return i / asin(wx - t5) + j / acos(wx + t6) + k / asin(wx - t5) + l / acos(wx + t6);
+    return I / asin(xz + t5) + J / acos(xz - t6) + K / asin(xz + t5) + L / acos(xz - t6);
+    return i / asin(xz - t5) + j / acos(xz + t6) + k / asin(xz - t5) + l / acos(xz + t6);
 }
 }
