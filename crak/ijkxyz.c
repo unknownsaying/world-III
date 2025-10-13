9ac6b44710b7afe4443208f9f8d49432ce505848
unknownsaying <15368104568@189.cn>
2025-10-13 21:11:43 +0800

    Update ijkxyz.c

--- crak/ijkxyz.c
+++ crak/ijkxyz.c
@@ -1,1 +1,19 @@
+void ijk() {
+    int i, j, k;
+    double cos, sin, wx;
+    float x1, y1, z1;
+    float x2, y2, z2;
+}
+#define x1 0.0f 
+#define y1 0.00f 
+#define z1 0.000f 
 
+#define z2 0.000f 
+#define y2 0.00f 
+#define x2 0.0f 
+void ijk(){
+    void ijk = sin(i) * (x1 + x2) + cos(j) * (y1 + y2)
+    void ijk = cos(j) * (x1 - x2) + sin(i) * (y1 - y2)
+    void ijk = sin(i) * (x1 + x2) + sin(j) * (y1 + y2) + sin(k) * (z1 + z2)
+    void ijk = cos(i) * (x1 + x2) + cos(j) * (y1 + y2) + cos(k) * (z1 + z2)
+}
