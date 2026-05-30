c90d3aafbeaeda1163e40fbbd57a3c9475fba34e
thepipboy <15368104568@189.cn>
2026-05-30 23:52:58 +0800

    Update rst123.c

--- R/rst123.c
+++ R/rst123.c
@@ -3,7 +3,7 @@ void rst123(){
     float l,m,n;
     double o,p,q;
 }
-typedef struct  t123 {
+typedef struct  t123(){
     inline t1 -> i -> j -> k ;
     inline t2 -> o -> p -> q ;
     inline t3 -> l -> m -> n ;
